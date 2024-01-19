33bc7cde0c0cd662c8fbfc0345caf580e78367ea
MobilGame06 <nico.schnass@gmail.com>
2024-01-19 20:45:43 +0100

    Update radioConfig.js

--- radioConfig.js
+++ radioConfig.js
@@ -27,20 +27,8 @@ const radioConfig = {
   "BIGFM": {
     url: "https://audiotainment-sw.streamabc.net/atsw-bigfm-aac-128-6355201?sABC=6541500o%230%231p6s83qso1sn2p12r4nr4o0189o876os%23gjy&aw_0_1st.playerid=twl&amsparams=playerid:twl;skey:1698779147"
   },
-  "1LIVE Diggi": {
-    url: "https://f121.rndfnk.com/ard/wdr/1live/diggi/mp3/128/stream.mp3?cid=01FBS02KK2TSYGZSY19X8ZJ3RK&sid=2XXd66lPgclRhGIDCqVxP4KBmWx&token=eL_cyc4ZqbHde_yQU2F0skxS3WalYwft8Pq7_1KwgGs&tvf=tZ9V-U5akxdmMTIxLnJuZGZuay5jb20"
-  },
   "Radio Bollerwagen": {
     url: "http://ffn-stream23.radiohost.de/radiobollerwagen_mp3-192?"
-  },
-  "SWR3": {
-    url: "https://f141.rndfnk.com/ard/swr/swr3/live/mp3/128/stream.mp3?aggregator=web&cid=01FC1X5J7PN2N3YQPZYT8YDM9M&sid=2XXdmaSKccz0MsmcUQPoSNYy40C&token=d4eg67xy_D9zxwVJLc-DjjqNtk0PZApJnZF5iQT6hcE&tvf=0YiNl51akxdmMTQxLnJuZGZuay5jb20"
-  },
-  "SWR1 RP": {
-    url: "https://d141.rndfnk.com/ard/swr/swr1/rp/mp3/128/stream.mp3?aggregator=web&cid=01FC1X3Q82Y5KQ31V7YJVJJ07W&sid=2XXdlfVvCME7FFeyJX5nHn32wDx&token=UMO4dbLAp3VMsSwGWxX_2s2bBfH2lDk59jh_hcg_UWI&tvf=XEuG7ptakxdkMTQxLnJuZGZuay5jb20"
-  },
-  "SWR1 BW": {
-    url: "https://d121.rndfnk.com/ard/swr/swr1/bw/mp3/128/stream.mp3?aggregator=web&cid=01FC1X3K2Z71SMDKMEC68DM7MW&sid=2XXdk1shem7qhm0pwEjQLluIkyI&token=FW5gaax_zNFtqcMIKvXEU4n4FmsI6aKZVdtsXyFc9iQ&tvf=CfDdBplakxdkMTIxLnJuZGZuay5jb20"
   },
   "ANTENNE BAYERN": {
     url: "https://s2-webradio.antenne.de/antenne/stream/mp3"
