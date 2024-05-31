e4c4efaabf5d93d2eda42341ca3d2313a6fb80a5
MobilGame06 <nico.schnass@gmail.com>
2024-05-31 22:27:28 +0200

    credits

--- src/commands/music/radioplay.js
+++ src/commands/music/radioplay.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandOptionType, ComponentType, ActionRowBuilder, SelectMenuBuilder, StringSelectMenuBuilder} = require("discord.js");
+const { ApplicationCommandOptionType, EmbedBuilder, ComponentType, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
 const { processPlayResult, updatePlayer, addMusicControls } = require("../../utilities/lavalink.js");
 const { loadChecks, playChecks } = require("../../utilities/checks.js");
 const { searchRadio } = require("../../utilities/radioApi.js");
@@ -35,46 +35,52 @@ module.exports = {
       value: station.url
     }));
     const selectMenu = new StringSelectMenuBuilder()
-        .setCustomId('radioSelect')
-        .setPlaceholder('Select a radio station')
-        .addOptions(selectMenuOptions)
-        .setMinValues(1)
-        .setMaxValues(1)     
-    
+      .setCustomId('radioSelect')
+      .setPlaceholder('Select a radio station')
+      .addOptions(selectMenuOptions)
+      .setMinValues(1)
+      .setMaxValues(1)
+
 
     const row = new ActionRowBuilder().addComponents(selectMenu);
 
-    const reply = await interaction.editReply({ content: 'Please select a radio station:', components: [row], ephemeral: true });
-    
+    const embed = new EmbedBuilder()
+      .setColor('#FF0000')
+      .setTitle('Please select a radio station')
+      .addFields({ name: 'Powered By:', value: '[RadioFM](https://www.radiofm.com)' })
+      .setTimestamp()
+
+    const reply = await interaction.editReply({ embeds: [embed], components: [row], ephemeral: true });
+
     const collector = reply.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
 
     collector.on('collect', async i => {
-        const selection = i.values[0];
-        await console.log(selection);
-        const player = interaction.client.lavalink.createPlayer({
-            guildId: interaction.guild.id,
-            voiceChannelId: interaction.member.voice.channel.id,
-            textChannelId: interaction.channel.id,
-            selfDeaf: true
-          });
-        const result = await player.search(selection, interaction.member);
-        if (!loadChecks(interaction, result)) { return }
+      const selection = i.values[0];
+      await console.log(selection);
+      const player = interaction.client.lavalink.createPlayer({
+        guildId: interaction.guild.id,
+        voiceChannelId: interaction.member.voice.channel.id,
+        textChannelId: interaction.channel.id,
+        selfDeaf: true
+      });
+      const result = await player.search(selection, interaction.member);
+      if (!loadChecks(interaction, result)) { return }
 
-        if (!player.connected) {
+      if (!player.connected) {
         if (!interaction.member.voice.channel) {
-            await player.destroy();
-            await interaction.editReply(errorEmbed('You need to be in a voice channel to use this command.'));
-            return;
+          await player.destroy();
+          await interaction.editReply(errorEmbed('You need to be in a voice channel to use this command.'));
+          return;
         }
         await player.connect();
-        }
+      }
 
-        const embed = await processPlayResult(player, result, interaction.client);
+      const embed = await processPlayResult(player, result, interaction.client);
 
-        updatePlayer(player, interaction.guild.id, interaction.client); 
-        const message = await interaction.editReply({ embeds: [embed], ephemeral: true });
-        await addMusicControls(message, player);
-        });
+      updatePlayer(player, interaction.guild.id, interaction.client);
+      const message = await interaction.editReply({ embeds: [embed], ephemeral: true });
+      await addMusicControls(message, player);
+    });
 
   }
 };
