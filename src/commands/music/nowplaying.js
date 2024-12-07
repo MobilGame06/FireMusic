d81a43afa4308a765d7a7ec4e02cbf9134d741c7
Mobil Game 06 <75528709+MobilGame06@users.noreply.github.com>
2024-12-07 15:33:43 +0100

    fixed now playing command (#11)

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -1,7 +1,7 @@
 const { ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
 const { updatePlayer, msToHMS, addMusicControls } = require("../../utilities/lavalink.js");
 const { genericChecks } = require("../../utilities/checks.js");
-const {  simpleEmbed } = require("../../utilities/embeds.js");
+const { simpleEmbed } = require("../../utilities/embeds.js");
 module.exports = {
   name: "nowplaying",
   description: 'shows the current playing track',
@@ -13,24 +13,21 @@ module.exports = {
     const track = player.queue.current
     const trackInfo = track.info
 
-    const progress = Math.round(20 * player.position / trackInfo.duration)
-    const progressBar = '▬'.repeat(progress) + '🔘'.repeat(20 - progress)
-
     const embed = new EmbedBuilder()
-        .setAuthor({ name: 'Now Playing...', iconURL: interaction.member.displayAvatarURL() })
-        .setTitle(trackInfo.title)
-        .setURL(trackInfo.uri)
-        .setThumbnail(trackInfo.artworkUrl)
-        .addFields([
-        { name: 'Duration', value: trackInfo.isStream ? '🔴 Live' : `\`${progressBar}\`\n\`${msToHMS(player.position)}/${msToHMS(trackInfo.duration)}\``, inline: true },
+      .setAuthor({ name: 'Now Playing...', iconURL: interaction.member.displayAvatarURL() })
+      .setTitle(trackInfo.title)
+      .setURL(trackInfo.uri)
+      .setColor("#ff0000")
+      .setThumbnail(trackInfo.artworkUrl)
+      .addFields([
+        { name: 'Duration', value: trackInfo.isStream ? '🔴 Live' : `${msToHMS(player.position)}/${msToHMS(trackInfo.duration)}`, inline: true },
         { name: 'Author', value: trackInfo.author, inline: true },
         { name: 'Requested By', value: track.requester.toString(), inline: true }
-        ])
-        //.setFooter({ text: `Kalliope | Repeat: ${formatRepeatMode(player.repeatMode)}`, iconURL: interaction.client.user.displayAvatarURL() })
+      ])
 
-        if (track.pluginInfo.uri) { embed.setDescription(`This track has been resolved on [Youtube](${track.pluginInfo.uri})`) }
+    if (track.pluginInfo.uri) { embed.setDescription(`This track has been resolved on [Youtube](${track.pluginInfo.uri})`) }
 
-        const message = await interaction.editReply({ embeds: [embed], fetchReply: true })
-        await addMusicControls(message, player)
-}
+    const message = await interaction.editReply({ embeds: [embed], fetchReply: true })
+    await addMusicControls(message, player)
+  }
 }
